1be20798abffd4c025f4ae94b0d061a0da375b1e
Amuthakandasamy <amudha.mail@gmail.com>
2025-09-10 18:12:03 +0530

    cmd

--- mrf.js
+++ mrf.js
@@ -12,9 +12,12 @@ const Arr = [2,3,4,5,6,7,8,9,10,12,13];
 //} ); 
 
 const outputMap = Arr.map((x) => x * 3); //map iterates each value and multiple with 3 and push in a new array 
+
 console.log(outputMap);
 
 const outputFilter = Arr.filter((x) => x < 10); //  iterates each values and filter less than 10 to a new array
+
+console.log(outputFilter);
 
 const outputreduce = Arr.reduce(function(prevNum, currNum) // iterates each values and return the sum of array values
 {
