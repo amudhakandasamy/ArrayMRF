764ceaa13153588eadc923acf6e0caf9348ebf5d
Amuthakandasamy <amudha.mail@gmail.com>
2025-09-10 21:45:58 +0530

    upt

--- mrfpractice.js
+++ mrfpractice.js
@@ -2,8 +2,8 @@
 
 
 // 1. [mangoe, orange, apple] add the word S to end of each string.
-// 2. [10,11,12,89,35,156,199,25,32,87] Find the numbers which are greater than 80 from given array.
-// 3. [10,12,15,16,8,11] Find the largest/ greates number in an array.
+
+
 
 const arr = ["apple","orange","mango"]
 const outputMap = arr.map((x) => x + "s"); 
@@ -12,7 +12,7 @@ console.log(arr);
 console.log("using Map plural the singular word");
 console.log(outputMap);
 
-
+// 2. [10,11,12,89,35,156,199,25,32,87] Find the numbers which are greater than 80 from given array.
 const arrNum = [10,11,12,89,35,156,199,25,32,87]
 const outputFltr= arrNum.filter((x) => x > 80);  
 
@@ -21,7 +21,9 @@ console.log("using Filter printing the value greater than 80");
 console.log(outputFltr);
 
 
-const arrRdc = [56,34,56,89,10,12,15,16,8,11]
+// 3. [10,12,15,16,8,11] Find the largest/ greates number in an array.
+
+const arrRdc = [10,12,15,16,8,11]
 
 const outputRed= arrRdc.reduce((pNumb , nxtNum) => {
   return pNumb < nxtNum ? nxtNum : pNumb;
