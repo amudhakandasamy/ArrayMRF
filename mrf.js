81cfbcc272d4c06604f3450f8800ea47eac2a68e
Amuthakandasamy <amudha.mail@gmail.com>
2025-09-10 19:31:35 +0530

    upt

--- mrf.js
+++ mrf.js
@@ -13,10 +13,12 @@ const Arr = [2,3,4,5,6,7,8,9,10,12,13];
 
 const outputMap = Arr.map((x) => x * 3); //map iterates each value and multiple with 3 and push in a new array 
 
+console.log("Map output");
 console.log(outputMap);
 
 const outputFilter = Arr.filter((x) => x < 10); //  iterates each values and filter less than 10 to a new array
 
+console.log("filter output");
 console.log(outputFilter);
 
 const outputreduce = Arr.reduce(function(prevNum, currNum) // iterates each values and return the sum of array values
@@ -25,4 +27,5 @@ const outputreduce = Arr.reduce(function(prevNum, currNum) // iterates each valu
     return prevNum;
 },0);
 
+console.log("reduce output");
 console.log(outputreduce);
