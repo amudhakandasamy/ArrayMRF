59b867e889dd636467e70eacce862a1a85fe94e8
Amuthakandasamy <amudha.mail@gmail.com>
2025-09-10 21:44:23 +0530

    upt

--- mrfpractice.js
+++ mrfpractice.js
@@ -8,22 +8,25 @@
 const arr = ["apple","orange","mango"]
 const outputMap = arr.map((x) => x + "s"); 
 
-console.log("Map output");
+console.log(arr);
+console.log("using Map plural the singular word");
 console.log(outputMap);
 
 
 const arrNum = [10,11,12,89,35,156,199,25,32,87]
 const outputFltr= arrNum.filter((x) => x > 80);  
 
-console.log("Filter output");
+console.log(arrNum);
+console.log("using Filter printing the value greater than 80");
 console.log(outputFltr);
 
 
-const arrRed = [56,34,56,89,10,12,15,16,8,11]
+const arrRdc = [56,34,56,89,10,12,15,16,8,11]
 
-const outputRed= arrRed.reduce((pNumb , nxtNum) => {
+const outputRed= arrRdc.reduce((pNumb , nxtNum) => {
   return pNumb < nxtNum ? nxtNum : pNumb;
 })
 
-console.log("reduce output");
+console.log(arrRdc);
+console.log("using reduce printing the greatest number in Array");
 console.log(outputRed);
